09bcce475844c626752b574690be90eb4e503255
hibiki <hibiki123123@gmail.com>
2021-11-15 16:20:45 +0900

    some add

--- main.go
+++ main.go
@@ -15,6 +15,13 @@ import (
 	"strings"
 )
 
+type OutputInfo struct {
+	OutputURLs []string `json:"outURLs"`
+	Stdout string `json:"stdout"`
+	Stderr string `json:"stderr"`
+	Startok bool `json:"startOK"`
+	Endok bool `json:"endOK"`
+}
 // NullWriter は何も書かない
 // これをloggerのSetOutputにセットしたらログを吐かない
 type NullWriter int
@@ -69,21 +76,6 @@ func Exec(command string) (stdoutStr string, stderrStr string, cmderr error) {
 
 	return
 }
-func execCommand(cmd *exec.Cmd) error {
-
-	var (
-		stdout bytes.Buffer
-		stderr bytes.Buffer
-	)
-
-	cmd.Stdout = &stdout
-	cmd.Stderr = &stderr
-
-	err := cmd.Run()
-
-	return err
-
-}
 
 // RequestBody はアップロードされた情報をサーバーに送る際の構造体
 type RequestBody struct {
@@ -96,12 +88,14 @@ type ResponseBody struct {
 	OutPathURLs []string `json:"outURLs"`
 	StdOut string `json:"stdout"`
 	StdErr string `json:"stderr"`
+	CmdStartSuccess bool `json:"cmdStartSuccess"`
+	CmdEndSuccess bool `json:"cmdEndSuccess"`
 }
 
 // processFileOnServerはサーバにアップロードしたファイルを処理させる。
 // サーバのurl, アップロードしたuploadedFile、サーバ上でコマンドを実行するためのparametaを受け取る
 // 返り値はサーバー内で出力したファイルを取得するためのURLパスのリストを返す。
-func processFileOnServer(url string, uploadedFile string, parameta string, myLogger *log.Logger) (ResponseBody, error) {
+func processFileOnServer(url string, uploadedFile string, parameta string, myLogger *log.Logger) (OutputInfo, error) {
 
 	myLogger.Printf("url: %v\n", url)
 	myLogger.Printf("uploadFile: %v\n", uploadedFile)
@@ -113,7 +107,7 @@ func processFileOnServer(url string, uploadedFile string, parameta string, myLog
 	requestBody, err := json.Marshal(reqBody)
 	myLogger.Printf("requestBody: %v\n", string(requestBody))
 	if err != nil {
-		return ResponseBody{}, err
+		return OutputInfo{}, err
 	}
 
 	body := bytes.NewReader(requestBody)
@@ -121,39 +115,43 @@ func processFileOnServer(url string, uploadedFile string, parameta string, myLog
 	// POSTリクエストを作成
 	req, err := http.NewRequest("POST", url, body)
 	if err != nil {
-		return ResponseBody{}, err
+		return OutputInfo{}, err
 	}
 	req.Header.Set("Content-Type", "application/json")
 
 	client := &http.Client{}
 	resp, err := client.Do(req)
 	if err != nil {
-		return ResponseBody{}, err
+		return OutputInfo{}, err
 	}
 
 	defer resp.Body.Close()
 
 	// レスポンスを受け取り、格納する。
-	var res ResponseBody
+	var res OutputInfo
 	b, err := ioutil.ReadAll(resp.Body)
 	myLogger.Printf("Response body: %v\r", string(b))
 	if err := json.Unmarshal(b, &res); err != nil {
 		log.Fatal(err)
 	}
+	myLogger.Printf("res: %v\n", res)
 
 	return res, err
 
 }
 
 func main() {
+	// example -> go run main.go -url http://127.0.0.1:8081 -name convertToJson -i test.txt -o ./out -p "-s ss -d dd" -l
 	var (
-		url       string
+		baseURL       string
+		proName string
 		inputFile string
 		outputDir string
 		parameta string
 		LogFlag bool
 	)
-	flag.StringVar(&url, "url", "default", "please url")
+	flag.StringVar(&baseURL, "url", "localhost:8081", "please url")
+	flag.StringVar(&proName, "name", "default program name", "please program name")
 	flag.StringVar(&inputFile, "i", "default in file", "please input file")
 	flag.StringVar(&outputDir, "o", "default out dir", "please output dir")
 	flag.StringVar(&parameta, "p", "default parameta", "please parameta")
@@ -166,7 +164,7 @@ func main() {
 	// ローカルファイルをサーバーにアップロードする
 	// curl -X POST -F "file=@<ファイル名>" localhost:8080/upload
 	myLogger.Println("-- File Upload to server --")
-	command := "curl -X POST -F file=@" + inputFile + " localhost:8080/upload"
+	command := "curl -X POST -F file=@" + inputFile + " " + baseURL + "/upload"
 	stdout, stderr, err := Exec(command)
 	myLogger.Printf("commands: %v\n", command)
 	myLogger.Printf("stdout: %v\n", stdout)
@@ -178,20 +176,17 @@ func main() {
 	// アップロードしたファイル情報を送信しサーバー上で処理する。
 	// サーバでの実行結果を表示する。
 	basename := filepath.Base(inputFile)
-	res, err := processFileOnServer(url, basename, parameta, myLogger)
-	if res.StdOut == "" {
-		fmt.Println(res.StdErr)
-	} else {
-		fmt.Println(res.StdOut)
-	}
+	proURL := baseURL + "/pro/" + proName
+	res, err := processFileOnServer(proURL, basename, parameta, myLogger)
+	fmt.Println(res.Stdout)
+	fmt.Println(res.Stderr)
 	if err != nil {
 		myLogger.Fatalln(err)
 	}
 
-
 	// processFileOnServer関数で処理されアウトプットされたファイルをcurlコマンドで取得する
-	myLogger.Println("-- Get File from server --")
-	for _, getOutFileURL := range res.OutPathURLs {
+	for _, getOutFileURL := range res.OutputURLs {
+		myLogger.Println("-- Get File from server --")
 		command := "curl -OL " + getOutFileURL
 		stdout, stderr, err = Exec(command)
 		myLogger.Printf("commands: %v\n", command)
